71de14050efffbb5e76e58afdefc91285d731f34
jcalabro <jamesrcalabro@gmail.com>
2024-03-11 09:20:35 -0400

    updating dotfiles

--- vim/config.lua
+++ vim/config.lua
@@ -22,34 +22,40 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
 })
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "[g", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]g", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 end)
 
 lsp.setup()
 
 local tele_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<Space>s', tele_builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<Space>f', tele_builtin.find_files, {})
+vim.keymap.set('n', '<Space>g', tele_builtin.live_grep, {})
+vim.keymap.set('n', '<Space>j', tele_builtin.jumplist, {})
+vim.keymap.set('n', '<Space>m', tele_builtin.man_pages, {})
+vim.keymap.set('n', '<Space>o', tele_builtin.oldfiles, {})
+vim.keymap.set('n', '<Space>r', tele_builtin.registers, {})
 vim.keymap.set('n', '<Space>\'', tele_builtin.resume, {})
 
 local actions = require("telescope.actions")
 require("telescope").setup{
-  defaults = {
-    mappings = {
-      i = {
-        ["<esc>"] = actions.close,
-        ["<C-q>"] = actions.close,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-      },
-    },
-  }
+    defaults = {
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close,
+                ["<C-q>"] = actions.close,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+            },
+        },
+    }
 }
