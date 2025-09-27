7ff984d9c34e59f8c471812090fb872644b3ac3b
jcalabro <jamesrcalabro@gmail.com>
2025-09-26 20:46:48 -0400

    updating dotfiles

--- linux/snap.sh
+++ linux/snap.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-sudo snap install ghostty --classic
 sudo snap install obsidian --classic
 sudo snap install code --classic
 sudo snap install 1password
 sudo snap install slack
 sudo snap install discord
 sudo snap install spotify
-sudo snap install chromium
